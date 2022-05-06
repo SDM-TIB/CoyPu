be52ff3f062dfa5235dffe005d35be64630c55af
vyasakhilesh <akh.vyas@gmail.com>
2022-05-06 18:44:35 +0200

    lei mappings

--- knowledge_graph_creation/dragoman.sh
+++ knowledge_graph_creation/dragoman.sh
@@ -2,7 +2,7 @@
 
 docker exec -it dragoman cp /knowledge_graph_creation/mapping_funcs/functions.py /app/Interpreter/
 
-curl localhost:6000/mapping_transformation/knowledge_graph_creation/configs/config_func.ini
+time curl localhost:6000/mapping_transformation/knowledge_graph_creation/configs/config_func.ini
 
 # move translated functional mapping files into mapping folder
 cp data/translated/*.ttl knowledge_graph_creation/mappings/
