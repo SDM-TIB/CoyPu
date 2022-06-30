b0a238a8801ba58d3fe665f292838d0ad577140d
vyasakhilesh <akh.vyas@gmail.com>
2022-06-30 16:42:50 +0200

    added test.py for testing dragoman and sdmrdfizer

---
+++ test/test.py
@@ -0,0 +1,7 @@
+import rdfizer
+from Interpreter.translate import translate
+
+
+translate('./knowledge_graph_creation/configs/config_func.ini')
+#rdfizer.semantify('./knowledge_graph_creation/configs/config.ini')
+
