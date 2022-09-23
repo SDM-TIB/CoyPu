c7c7aa12b4cd3b9a22ca7c6e0902b6238775f43a
vyasakhilesh <akh.vyas@gmail.com>
2022-09-23 10:09:24 +0200

    query edited

--- python/query/queries.py
+++ python/query/queries.py
@@ -386,6 +386,11 @@ WHERE {
     ?itemP ps:P1082 ?population .
 }
 """
+query_4_fdq_ex_sql = """SELECT isocode, country, year, SUM(fatalities*1000000/population) AS fatalities_per_million
+FROM `query_4_fdq_ex`
+WHERE YEAR(timestamp)=year
+GROUP BY year, isocode, country
+"""
 
 
 
